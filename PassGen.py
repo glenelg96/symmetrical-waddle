5a9794324ffb428f2f50ded680d70d74f9bdcb7b
Glen Coulter <glencoulter@cr-admins-MBP-4.edi.cloudreach>
2018-07-10 13:40:39 +0100

    first commit

---
+++ PassGen.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+Password Generator
+"""
+__author__ = "Glen Coulter"
+__version__ = "0.1"
+__license__ = MIT
+
+import random
+import string
+def main():
+    charset = string.printable
+    length = input("Please input password length: ")
+    paswd=""
+    for n in range(length):
+        ni = random.randrange(len(charset))
+        paswd = paswd + charset[ni]
+    print(paswd)
+if __name__ == "__main__":
+    main()
